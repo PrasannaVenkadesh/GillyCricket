ca066dde2e4c6a62e96e8f2a752eac2788601f72
Prasanna Venkadesh <joinwhizchat@gmail.com>
2012-04-16 00:20:57 +0530

    Uninstallation added

--- ubuntu_install.sh
+++ ubuntu_install.sh
@@ -1,6 +1,7 @@
 #! /bin/bash
 apt-get install python-feedparser python-tk
-chmod +x Gilly Gillylive
-cp Gilly Gillylive /opt/
-ln -s /opt/Gilly /usr/bin/Gilly
-ln -s /opt/Gillylive /usr/bin/Gillylive
+chmod +x Gilly.py Gillylive.py removeGilly.sh
+cp Gilly.py Gillylive.py removeGilly.sh /opt/
+ln -s /opt/Gilly.py /usr/bin/Gilly
+ln -s /opt/Gillylive.py /usr/bin/Gillylive
+ln -s /opt/removeGilly.sh /usr/bin/removeGilly
--- uninstall.sh
+++
@@ -1,6 +0,0 @@
-#! /bin/bash
-
-rm /usr/bin/Gilly /opt/Gilly
-rm /usr/bin/Gillylive /opt/Gillylive
-
-echo "Completed!!! All files have been removed!"
